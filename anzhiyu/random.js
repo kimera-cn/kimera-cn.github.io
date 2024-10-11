bdccd6562f5d970680ee8e125ec999a865af5f10
kimera428 <2965614309@qq.com>
2024-10-11 13:23:31 +0800

    Site updated: 2024-10-11 13:23:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/10/hello-world/"];function toRandomPost(){
+var posts=["2024/10/11/我的第一篇文章/","2024/10/10/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
