86d63ca77c926eca4393487c0f5b3c2e96d65057
kimera428 <2965614309@qq.com>
2024-10-11 17:09:33 +0800

    Site updated: 2024-10-11 17:09:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/11/我的第一篇文章/","2024/10/11/JavaScript之操作元素/"];function toRandomPost(){
+var posts=["2024/10/11/JavaScript之操作元素/","2024/10/11/我的第一篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
