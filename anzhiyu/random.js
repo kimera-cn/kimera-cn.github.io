4b8575f7f19eb5000139200b0eba67e327b3faab
kimera428 <2965614309@qq.com>
2024-10-16 16:22:33 +0800

    Site updated: 2024-10-16 16:22:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/11/JavaScript之操作元素/","2024/10/11/我的第一篇文章/"];function toRandomPost(){
+var posts=["2024/10/11/我的第一篇文章/","2024/10/12/JavaScript-DOM/","2024/10/11/JavaScript之操作元素/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
