6ee6789211c514d750256de761575f658a90ba12
kimera428 <2965614309@qq.com>
2024-11-21 20:19:44 +0800

    Site updated: 2024-11-21 20:19:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/12/JavaScript-DOM/","2024/11/16/事件高级/","2024/10/11/我的第一篇文章/"];function toRandomPost(){
+var posts=["2024/10/11/我的第一篇文章/","2024/10/12/JavaScript-DOM/","2024/11/16/事件高级/","2024/11/21/Vue生命周期/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
